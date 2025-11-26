fca85fad00da01cecc91d4b39aa6ceb361661a23
Yash Puniwala <yashpuniwala@gmail.com>
2025-11-26 08:55:56 +0530

    first commit

---
+++ src/payload.config.ts
@@ -0,0 +1,53 @@
+// storage-adapter-import-placeholder
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
+import { multiTenantPlugin } from "./../node_modules/@payloadcms/plugin-multi-tenant/dist/index";
+import { Media } from "./collections/Media";
+import { Users } from "./collections/Users";
+import { Categories } from "./collections/Categories";
+import { Products } from "./collections/Products";
+import { Tags } from "./collections/Tags";
+import { Tenants } from "./collections/Tenants";
+import { Orders } from "./collections/Orders";
+import { Reviews } from "./collections/Reviews";
+import { isSuperAdmin } from "./lib/access";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media, Categories, Products, Tags, Tenants, Orders, Reviews],
+  cookiePrefix: "funroad",
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || "",
+  }),
+  sharp,
+  plugins: [
+    payloadCloudPlugin(),
+    multiTenantPlugin({
+      collections: {
+        products: {},
+      },
+      tenantsArrayField: {
+        includeDefaultField: false,
+      },
+      userHasAccessToAllTenants: (user) => isSuperAdmin(user)}),
+    // storage-adapter-placeholder
+  ],
+});
