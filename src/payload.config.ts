15d4d11ce25685d8875ca29450ca51ec3fb96edd
Yash Puniwala <yashpuniwala@gmail.com>
2025-12-03 08:06:34 +0530

    Added vercelBlobStorage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,6 +16,7 @@ import { Tenants } from "./collections/Tenants";
 import { Orders } from "./collections/Orders";
 import { Reviews } from "./collections/Reviews";
 import { isSuperAdmin } from "./lib/access";
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -61,6 +62,13 @@ export default buildConfig({
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      clientUploads: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN
+    })
   ],
 });
